9c5cb19f98c2e7894fa3f7c6ae92f9cf79385cf7
ferrisboran <ferris@ferryrules.com>
2019-04-23 18:55:35 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +8,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+#index action
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+#new action
+  get '/articles/new' do
+    erb :new
+  end
+
+#show action
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article[:id]}"
+  end
+
+#edit action
+  get '/articles/:id/edit' do
+    @articles = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @articles = Article.find(params[:id])
+    @articles.update(title: params[:title], content: params[:content])
+    erb :show
+  end
+
+#delete action
+  delete "/articles/:id" do
+    @articles = Article.find(params[:id])
+    @articles.destroy
+    redirect '/articles'
+  end
+
 end
